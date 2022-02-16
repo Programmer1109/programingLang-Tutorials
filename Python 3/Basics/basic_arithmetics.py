e72da1af3c0bb73b7ba77c1a5565d92d2a660bcd
Anthony D'souza <jordananthony786@gmail.com>
2022-02-16 11:54:31 +0530

    Basic Add, Sub, etc.
    Learn how to perform basic arithmetic operations like add, subtract, multiply, divide, etc

---
+++ Python 3/Basics/basic_arithmetics.py
@@ -0,0 +1,20 @@
+a = 5
+b = 6
+# Addition of two numbers
+add = a+b
+print(f"Addition = {add}")  # Formatted output string
+# Differnce of two numbers
+diff = a-b
+print(f"Subtraction = {diff}")
+# Product of two numbers
+multiply = a*b
+print(f"Multiplication = {multiply}")
+# Division of two numbers
+div = a/b
+print(f"Division = {div}")
+# Modulo Division of two numbers
+mod_div = a//b
+print(f"Subtraction = {mod_divi}")
+# Exponentiation of two numbers
+expo = a**b
+print(f"Subtraction = {expo}")
