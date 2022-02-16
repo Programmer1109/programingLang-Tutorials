2309ba0909a46b89b158e4fbffcd9cfd5c89354b
Anthony D'souza <jordananthony786@gmail.com>
2022-02-16 12:02:54 +0530

    taking input from user

---
+++ Python 3/Basics/User_input.py
@@ -0,0 +1,9 @@
+# input in python is by default String input
+name = input(print("Enter your Name:- ")
+print(f"Hello {name}, Nice to meet you!!!")
+# numeric input in python      
+integer = int(input(print("Enter your lucky int:- ")))
+print(f"{name} your lucky number is {integer}.")
+floating = float(input(print("Enter a floating point number:- ")))             
+print(f"Entered number is {floating}.")
+    
