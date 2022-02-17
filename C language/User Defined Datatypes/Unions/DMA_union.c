792e434cfbca1463163f6a5cf45e10bd03b28b43
Anthony D'souza <jordananthony786@gmail.com>
2022-02-17 14:23:36 +0530

    Dynamic Memory Allocation of Union

--- C language/User Defined Datatypes/Unions/DMA_union.c
+++ C language/User Defined Datatypes/Unions/DMA_union.c
@@ -1,56 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// Dynamic Memory Allocation of Structure
-struct player{
+// Dynamic Memory Allocation of Union
+union player{
     int age;
     char name[50];
     int jerseyNo;
 };
 
 int main(){
-    struct player* footballer1;
-    struct player* footballer2;
+    union player* footballer1;
+    union player* footballer2;
 
-    // Pointer points to footballer 1
-    footballer1 = (struct player*) malloc(sizeof(struct player));
-    if(footballer1 == NULL){
+    footballer1 = (union player*) malloc(sizeof(union player));
+    if (footballer1 == NULL){
         printf("Error:- Running out of Memory!!!");
-        exit();
+        exit(0);
     }
-    //Enter details for player 1
-    printf("Enter details:-");
-    printf("\n\tName:- ");
-    fflush(stdin);
+    printf("Enter footballer name:- ");
     gets((*footballer1).name);
-    printf("\tAge:- ");
-    scanf("%d", &(*footballer1).age);
-    printf("\tJersey No.:- ");
-    scanf("%d", &(*footballer1).jerseyNo);
 
-    // Pointer points to footballer 2
-    footballer2 = (struct player*) malloc(sizeof(struct player));
-    if(footballer2 == NULL){
+    footballer2 = (union player*) malloc(sizeof(union player));
+    if (footballer2 == NULL){
         printf("Error:- Running out of Memory!!!");
-        exit();
+        exit(0);
     }
-    //Enter details for player 2
-    printf("\nEnter details:-");
-    printf("\n\tName:- ");
-    fflush(stdin);
-    gets(footballer2->name);
-    printf("\tAge:- ");
-    scanf("%d", &footballer2->age);
-    printf("\tJersey No.:- ");
-    scanf("%d", &footballer2->jerseyNo);
+    (*footballer2).age = 30;
 
-    printf("Output:-");
-    printf("\n\tName:- %s   Age:- %d   Jersey No.:- %d", (*footballer1).name, (*footballer1).age, (*footballer1).jerseyNo);
-    printf("\n\tName:- %s   Age:- %d   Jersey No.:- %d", footballer2->name, footballer2->age, footballer2->jerseyNo);
+    printf("OUTPUT:-\n");
+    printf("Footballer's Details:-\n\tname = %s  age = %d  jersey No. = %d\n", (*footballer1).name, (*footballer1).age, (*footballer1).jerseyNo);
+    printf("Footballer's Details:-\n\tname = %s  age = %d  jersey No. = %d\n", footballer2->name, footballer2->age, footballer2->jerseyNo);
 
     free(footballer1);
     free(footballer2);
-
     return 0;
 }
-
