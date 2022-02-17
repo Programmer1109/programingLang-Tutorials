fdd111ccd3c9cc8c538837e9907cfca01bd08e87
Anthony D'souza <jordananthony786@gmail.com>
2022-02-17 14:26:27 +0530

    Typedef with Structure

---
+++ C language/User Defined Datatypes/Structures/typedef.c
@@ -0,0 +1,37 @@
+// Typedef Keywords
+struct player{
+    int age;
+    char name[50];
+    int jerseyNo;
+};
+
+int main(){
+    typedef struct player football;
+    football footballer1;
+    football footballer2;
+    int size=50;
+    //Enter details for player 1
+    printf("Enter details:-");
+    printf("\n\tName:- ");
+    fflush(stdin);
+    gets(footballer1.name);
+    printf("\tAge:- ");
+    scanf("%d", &footballer1.age);
+    printf("\tJersey No.:- ");
+    scanf("%d", &footballer1.jerseyNo);
+    //Enter details for player 2
+    printf("\nEnter details:-");
+    printf("\n\tName:- ");
+    fflush(stdin);
+    gets(footballer2.name);
+    printf("\tAge:- ");
+    scanf("%d", &footballer2.age);
+    printf("\tJersey No.:- ");
+    scanf("%d", &footballer2.jerseyNo);
+
+    printf("Output:-");
+    printf("\n\tName:- %s   Age:- %d   Jersey No.:- %d", footballer1.name, footballer1.age, footballer1.jerseyNo);
+    printf("\n\tName:- %s   Age:- %d   Jersey No.:- %d", footballer2.name, footballer2.age, footballer2.jerseyNo);
+
+    return 0;
+}
