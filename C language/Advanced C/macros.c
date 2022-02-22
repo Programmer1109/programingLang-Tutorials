65f4784f210fea7f8dc69793cd5596644cb87c14
Anthony D'souza <jordananthony786@gmail.com>
2022-02-22 12:34:09 +0530

    Macros in C
    Using macros in C Programming Language

--- C language/Advanced C/macros.c
+++ C language/Advanced C/macros.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
 #include<stdlib.c>
-#define 
+
+// Macros in C Language
+    // Object like Macro
+#define Date 31
+    // Chain like Macro
+#define INSTAGRAM FOLLOWERS
+#define FOLLOWERS 138
+    // Function like Macro
+#define AREA(l, b) (l*b)
+    // Multi Line Macro
+#define Elements 15, \
+            23, \
+            37
+
 int main(){
-      
-  return 0;
+    int length=8, breadth=16, area;
+    int arr[] = {Elements};
+    printf("Lockdown will be extended upto %d-MAY-2020\n", Date);
+    printf("Geeks for Geeks have %dK followers on Instagram\n", INSTAGRAM);
+    area = AREA(length, breadth);
+    printf("Area = %d\n", area);
+    for(int i=0; i<3; i++)
+        printf("Element[%d] = %d\t", i, arr[i]);
+    return 0;
 }
