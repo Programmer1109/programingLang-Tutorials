2ce2adaa698993093bc52ddd869a47497388d6b3
Anthony D'souza <jordananthony786@gmail.com>
2022-02-23 15:00:34 +0530

    Preprocessor macros in C

--- C language/Advanced C/macros.c
+++ C language/Advanced C/macros.c
@@ -2,6 +2,11 @@
 #include<stdlib.c>
 
 // Macros in C Language
+    // Stringizing Operator
+#define stringer(x) printf("%s\t", #x)
+#define message(x, y) printf("%s -> %s", #x, #y)
+    // Token Passing Operator
+#define combine(x, y) x##y
     // Object like Macro
 #define Date 31
     // Chain like Macro
@@ -14,14 +19,19 @@
             23, \
             37
 
-int main(){
+int main() {
+    int abc = 30;
     int length=8, breadth=16, area;
     int arr[] = {Elements};
-    printf("Lockdown will be extended upto %d-MAY-2020\n", Date);
+    stringer(Jeg spiser ikke en pasta og drikker ool.);
+    stringer(De viser oss en hund og en katt.);
+    message(Min heter, Anthony);
+    printf("\nResult = %d", combine(ab, c));
+    printf("\nLockdown will be extended upto %d-MAY-2020\n", Date);
     printf("Geeks for Geeks have %dK followers on Instagram\n", INSTAGRAM);
     area = AREA(length, breadth);
     printf("Area = %d\n", area);
     for(int i=0; i<3; i++)
         printf("Element[%d] = %d\t", i, arr[i]);
-    return 0;
+   return 0;
 }
